781febc3475b9242d60f010a3e678c381d9f1bd0
Paul Stearns <celestialcoyote@gmail.com>
2023-01-25 15:59:55 -0800

    Added production serverand webpack build script

---
+++ webpack.common.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    context: path.resolve(__dirname, "src"),
+    entry: './index.js',
+    resolve: {
+        extensions: ['.js', ',jsx', '.json', '.ts', '.tsx', '.scss']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader',
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Production',
+            template: path.join(__dirname, 'public', 'index.html')
+        }),
+    ],
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+        clean: true,
+    }
+};
---
+++ webpack.dev.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        port: '5000',
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        open: true,
+        hot: true,
+        liveReload: true,
+        historyApiFallback: true
+    }
+});
